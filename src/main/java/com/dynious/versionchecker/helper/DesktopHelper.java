3f23673d14e10840838aebf72b93dc429bfee8f4
Keridos <fuffzehn@gmail.com>
2015-06-10 03:00:04 +0200

    fixing file and web browser support for Linux/Unix

--- src/main/java/com/dynious/versionchecker/helper/DesktopHelper.java
+++ src/main/java/com/dynious/versionchecker/helper/DesktopHelper.java
@@ -5,24 +5,46 @@ import org.lwjgl.Sys;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Logger;
 
-public class DesktopHelper
-{
+public class DesktopHelper {
     public static final File MOD_FOLDER;
 
-    static
-    {
+    static {
         MOD_FOLDER = new File("mods");
     }
-    public static void openFolderInExplorer(File file)
-    {
-        try
-        {
-            Desktop.getDesktop().browse(file.toURI());
-        }
-        catch (IOException e)
-        {
-            Sys.openURL("file://" + file.getAbsolutePath());
+
+    public static void openFolderInExplorer(File file) {
+        String[] fileBrowsers = {"gentoo", "xfe", "nautilus", "dolphin",
+                "andromeda", "pcmanfm", "spacefm", "thunar", "caja",
+                "dino", "xdg-open"};
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop().browse(file.toURI());
+            } catch (IOException e) {
+                Sys.openURL("file://" + file.getAbsolutePath());
+            }
+        } else {
+            String osName = System.getProperty("os.name");
+            try {
+                if (osName.contains("nix") || osName.contains("nux")) {
+                    boolean found = false;
+                    for (String fileBrowser : fileBrowsers)
+                        if (!found) {
+                            found = Runtime.getRuntime().exec(
+                                    new String[]{"which", fileBrowser}).waitFor() == 0;
+                            if (found)
+                                Runtime.getRuntime().exec(new String[]{fileBrowser, file.getAbsolutePath()});
+                        }
+                    if (!found)
+                        throw new Exception(Arrays.toString(fileBrowsers));
+                }
+            } catch (Exception e) {
+                Logger.getGlobal().severe("Could not open File Browser properly, please report your file browser and your OS to the Version Checker Github!");
+            }
         }
     }
 }
--- src/main/java/com/dynious/versionchecker/helper/WebHelper.java
+++ src/main/java/com/dynious/versionchecker/helper/WebHelper.java
@@ -9,121 +9,111 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.*;
+import java.util.Arrays;
+import java.util.logging.Logger;
 
-public class WebHelper
-{
-    public static void openWebpage(URI uri)
-    {
+public class WebHelper {
+    public static void openWebpage(URI uri) {
         Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE))
-        {
-            try
-            {
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
                 desktop.browse(uri);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    public static void openWebpage(URL url)
-    {
-        try
-        {
-            openWebpage(url.toURI());
-        }
-        catch (URISyntaxException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    public static void openWebpage(String string)
-    {
-        try
-        {
-            openWebpage(new URI(string));
-        }
-        catch (URISyntaxException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    public static boolean downloadUpdate(Update update)
-    {
-        ModContainer mod = ModHelper.getModContainer(update.MOD_ID);
-        if (mod != null && mod.getSource() != null && mod.getSource().isFile())
-        {
-            String fileName = "";
-            if (update.newFileName != null && !update.newFileName.isEmpty())
-            {
-                fileName = mod.getSource().getParent() + File.separator + update.newFileName;
+        } else {
+            String[] browsers = {"firefox", "opera", "konqueror", "epiphany",
+                    "seamonkey", "galeon", "kazehakase", "mozilla", "netscape",
+                    "google-chrome", "google-chrome-stable"};
+            String osName = System.getProperty("os.name");
+            try {
+                if (osName.contains("nix") || osName.contains("nux")) {
+                    boolean found = false;
+                    for (String browser : browsers)
+                        if (!found) {
+                            found = Runtime.getRuntime().exec(
+                                    new String[]{"which", browser}).waitFor() == 0;
+                            if (found)
+                                Runtime.getRuntime().exec(new String[]{browser, uri.toURL().toString()});
+                        }
+                    if (!found)
+                        throw new Exception(Arrays.toString(browsers));
+                }
+            } catch (Exception e) {
+                Logger.getGlobal().severe("Could not open URI properly, please report your browser and your OS to the Version Checker Github!");
             }
-            else
-            {
+
+        }
+    }
+
+    public static void openWebpage(URL url) {
+        try {
+            openWebpage(url.toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void openWebpage(String string) {
+        try {
+            openWebpage(new URI(string));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean downloadUpdate(Update update) {
+        ModContainer mod = ModHelper.getModContainer(update.MOD_ID);
+        if (mod != null && mod.getSource() != null && mod.getSource().isFile()) {
+            String fileName = "";
+            if (update.newFileName != null && !update.newFileName.isEmpty()) {
+                fileName = mod.getSource().getParent() + File.separator + update.newFileName;
+            } else {
                 fileName = mod.getSource().getAbsolutePath();
                 String newFileName = fileName.replace(update.oldVersion, update.newVersion);
-                if (fileName.equalsIgnoreCase(newFileName))
-                {
+                if (fileName.equalsIgnoreCase(newFileName)) {
                     int i = newFileName.lastIndexOf(".");
-                    if (i == -1)
-                    {
+                    if (i == -1) {
                         newFileName += "-new";
-                    }
-                    else
-                    {
+                    } else {
                         newFileName = newFileName.substring(0, i) + "-new" + newFileName.substring(i);
                     }
                 }
                 fileName = newFileName;
             }
-            try
-            {
+            try {
                 URL url = new URL(update.updateURL);
                 File file = downloadFileFromURL(url, update, mod, fileName);
                 if (file != null && file.exists() && file.length() > 0)
                     return true;
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
         return false;
     }
 
-    public static File downloadFileFromURL(URL url, Update update, ModContainer mod, String fileName) throws IOException
-    {
+    public static File downloadFileFromURL(URL url, Update update, ModContainer mod, String fileName) throws IOException {
         File newFile = new File(fileName);
         FileUtils.copyURLToFile(url, newFile);
         return newFile;
     }
 
-    public static String getLatestFilenameFromCurse(String urlString)
-    {
-        try
-        {
-            while (urlString != null && !urlString.isEmpty())
-            {
+    public static String getLatestFilenameFromCurse(String urlString) {
+        try {
+            while (urlString != null && !urlString.isEmpty()) {
                 URL url = new URL(urlString);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setInstanceFollowRedirects(false);
                 urlString = connection.getHeaderField("Location");
 
-                if (urlString != null && (urlString.endsWith(".jar") || urlString.endsWith(".zip")))
-                {
+                if (urlString != null && (urlString.endsWith(".jar") || urlString.endsWith(".zip"))) {
                     return urlString.substring(urlString.lastIndexOf("/") + 1);
                 }
             }
-        }
-        catch (MalformedURLException e)
-        {
+        } catch (MalformedURLException e) {
             LogHandler.error("Malformed URL was given when searching in Curse database!");
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
